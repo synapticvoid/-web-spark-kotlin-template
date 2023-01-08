4a536c2a04a523feda3ceb8a75ada95670319dcc
Robin Penea <robin.penea@gmail.com>
2023-01-08 16:07:59 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.21"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation("com.sparkjava:spark-core:2.9.4")
+
+    // Template engine
+    implementation("com.sparkjava:spark-template-velocity:2.7.1")
+
+    // Logging facility
+    implementation("org.slf4j:slf4j-simple:2.0.5")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
